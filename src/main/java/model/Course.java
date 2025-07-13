a67a9ba93ebf9d3f7a5fd509306aee9967855548
Tên của bạn <HuyNTCE182349@fpt.edu.vn>
2025-07-14 02:19:32 +0700

    Manage Courses for ad

--- src/main/java/model/Course.java
+++ src/main/java/model/Course.java
@@ -1,11 +1,9 @@
 package model;
 
 import java.sql.Timestamp;
+import java.util.List;
 import util.ImageBase64;
 
-/**
- * @author Ngo Phuoc Thinh - CE170008 - SE1815
- */
 public class Course {
     private int courseID;
     private String courseName;
@@ -21,7 +19,8 @@ public class Course {
     private String courseSummary;
     private String courseHighlight;
     private int status;
-    private  int totalEnrolled;
+    private int totalEnrolled;
+    private List<Module> modules;
 
     public Course() {
     }
@@ -31,17 +30,15 @@ public class Course {
         this.courseName = courseName;
     }
 
-    
-    
     public Course(int courseID, String courseName, User user) {
         this.courseID = courseID;
         this.courseName = courseName;
         this.user = user;
     }
 
-    
-
-    public Course(int courseID, String courseName, Category category, User user, int approveStatus, Timestamp publicDate, Timestamp courseLastUpdate, int salePrice, int originalPrice, int isSale, byte[] courseImageLocation, String courseSummary, String courseHighlight, int status, int totalEnrolled) {
+    public Course(int courseID, String courseName, Category category, User user, int approveStatus, Timestamp publicDate,
+                  Timestamp courseLastUpdate, int salePrice, int originalPrice, int isSale, byte[] courseImageLocation,
+                  String courseSummary, String courseHighlight, int status, int totalEnrolled, List<Module> modules) {
         this.courseID = courseID;
         this.courseName = courseName;
         this.category = category;
@@ -57,138 +54,43 @@ public class Course {
         this.courseHighlight = courseHighlight;
         this.status = status;
         this.totalEnrolled = totalEnrolled;
+        this.modules = modules;
     }
 
-    public Course(int courseID, String courseName, Category category, User user, int approveStatus, Timestamp publicDate, Timestamp courseLastUpdate, byte[] courseImageLocation) {
-        this.courseID = courseID;
-        this.courseName = courseName;
-        this.category = category;
-        this.user = user;
-        this.approveStatus = approveStatus;
-        this.publicDate = publicDate;
-        this.courseLastUpdate = courseLastUpdate;
-        this.courseImageLocation = courseImageLocation;
-    }
-
-    public int getCourseID() {
-        return courseID;
-    }
-
-    public void setCourseID(int courseID) {
-        this.courseID = courseID;
-    }
-
-    public String getCourseName() {
-        return courseName;
-    }
-
-    public void setCourseName(String courseName) {
-        this.courseName = courseName;
-    }
-
-    public Category getCategory() {
-        return category;
-    }
-
-    public void setCategory(Category category) {
-        this.category = category;
-    }
-
-    public User getUser() {
-        return user;
-    }
-
-    public void setUser(User user) {
-        this.user = user;
-    }
-
-    public int getApproveStatus() {
-        return approveStatus;
-    }
-
-    public void setApproveStatus(int approveStatus) {
-        this.approveStatus = approveStatus;
-    }
-
-    public Timestamp getPublicDate() {
-        return publicDate;
-    }
-
-    public void setPublicDate(Timestamp publicDate) {
-        this.publicDate = publicDate;
-    }
-
-    public Timestamp getCourseLastUpdate() {
-        return courseLastUpdate;
-    }
-
-    public void setCourseLastUpdate(Timestamp courseLastUpdate) {
-        this.courseLastUpdate = courseLastUpdate;
-    }
-
-    public int getSalePrice() {
-        return salePrice;
-    }
-
-    public void setSalePrice(int salePrice) {
-        this.salePrice = salePrice;
-    }
-
-    public int getOriginalPrice() {
-        return originalPrice;
-    }
-
-    public void setOriginalPrice(int originalPrice) {
-        this.originalPrice = originalPrice;
-    }
-
-    public int getIsSale() {
-        return isSale;
-    }
-
-    public void setIsSale(int isSale) {
-        this.isSale = isSale;
-    }
-
-    public byte[] getCourseImageLocation() {
-        return courseImageLocation;
-    }
-
-    public void setCourseImageLocation(byte[] courseImageLocation) {
-        this.courseImageLocation = courseImageLocation;
-    }
-
-    public String getCourseSummary() {
-        return courseSummary;
-    }
-
-    public void setCourseSummary(String courseSummary) {
-        this.courseSummary = courseSummary;
-    }
-
-    public String getCourseHighlight() {
-        return courseHighlight;
-    }
-
-    public void setCourseHighlight(String courseHighlight) {
-        this.courseHighlight = courseHighlight;
-    }
-
-    public int getStatus() {
-        return status;
-    }
-
-    public void setStatus(int status) {
-        this.status = status;
-    }
-
-    public int getTotalEnrolled() {
-        return totalEnrolled;
-    }
-
-    public void setTotalEnrolled(int totalEnrolled) {
-        this.totalEnrolled = totalEnrolled;
-    }
+    // Getters and Setters
+    public int getCourseID() { return courseID; }
+    public void setCourseID(int courseID) { this.courseID = courseID; }
+    public String getCourseName() { return courseName; }
+    public void setCourseName(String courseName) { this.courseName = courseName; }
+    public Category getCategory() { return category; }
+    public void setCategory(Category category) { this.category = category; }
+    public User getUser() { return user; }
+    public void setUser(User user) { this.user = user; }
+    public int getApproveStatus() { return approveStatus; }
+    public void setApproveStatus(int approveStatus) { this.approveStatus = approveStatus; }
+    public Timestamp getPublicDate() { return publicDate; }
+    public void setPublicDate(Timestamp publicDate) { this.publicDate = publicDate; }
+    public Timestamp getCourseLastUpdate() { return courseLastUpdate; }
+    public void setCourseLastUpdate(Timestamp courseLastUpdate) { this.courseLastUpdate = courseLastUpdate; }
+    public int getSalePrice() { return salePrice; }
+    public void setSalePrice(int salePrice) { this.salePrice = salePrice; }
+    public int getOriginalPrice() { return originalPrice; }
+    public void setOriginalPrice(int originalPrice) { this.originalPrice = originalPrice; }
+    public int getIsSale() { return isSale; }
+    public void setIsSale(int isSale) { this.isSale = isSale; }
+    public byte[] getCourseImageLocation() { return courseImageLocation; }
+    public void setCourseImageLocation(byte[] courseImageLocation) { this.courseImageLocation = courseImageLocation; }
+    public String getCourseSummary() { return courseSummary; }
+    public void setCourseSummary(String courseSummary) { this.courseSummary = courseSummary; }
+    public String getCourseHighlight() { return courseHighlight; }
+    public void setCourseHighlight(String courseHighlight) { this.courseHighlight = courseHighlight; }
+    public int getStatus() { return status; }
+    public void setStatus(int status) { this.status = status; }
+    public int getTotalEnrolled() { return totalEnrolled; }
+    public void setTotalEnrolled(int totalEnrolled) { this.totalEnrolled = totalEnrolled; }
+    public List<Module> getModules() { return modules; }
+    public void setModules(List<Module> modules) { this.modules = modules; }
+    public String getImageDataURI() { return ImageBase64.toDataURI(courseImageLocation, "image/jpeg"); }
 
     @Override
     public String toString() {
@@ -208,10 +110,7 @@ public class Course {
                 ", courseHighlight='" + courseHighlight + '\'' +
                 ", status=" + status +
                 ", totalEnrolled=" + totalEnrolled +
+                ", modules=" + modules +
                 '}';
-    }
-
-    public String getImageDataURI() {
-        return ImageBase64.toDataURI(courseImageLocation, "image/jpeg");
     }
 }
--- src/main/java/model/Material.java
+++ src/main/java/model/Material.java
@@ -1,164 +1,164 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package model;
+    /*
+     * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+     * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+     */
+    package model;
 
-import java.sql.Timestamp;
-import util.ImageBase64;
+    import java.sql.Timestamp;
+    import util.ImageBase64;
 
-/**
- *
- * @author Duy - CE180230 - SE1815
- */
-public class Material {
+    /**
+     *
+     * @author Duy - CE180230 - SE1815
+     */
+    public class Material {
 
-    private int materialId;
-    private String materialName;
-    private Module module;
-    private String type;
-    private Timestamp materialLastUpdate;
-    private int materialOrder;
-    private String time;
-    private String materialDescription;
-    private String materialUrl;
-    private byte[] materialFile;
-    private String fileName;
-    private String pdfDataURI;
+        private int materialId;
+        private String materialName;
+        private Module module;
+        private String type;
+        private Timestamp materialLastUpdate;
+        private int materialOrder;
+        private String time;
+        private String materialDescription;
+        private String materialUrl;
+        private byte[] materialFile;
+        private String fileName;
+        private String pdfDataURI;
 
-    public String getPdfDataURI() {
-        return pdfDataURI;
+        public String getPdfDataURI() {
+            return pdfDataURI;
+        }
+
+        public void setPdfDataURI(String pdfDataURI) {
+            this.pdfDataURI = pdfDataURI;
+        }
+
+        public Material() {
+        }
+
+        public String getFileName() {
+            return fileName;
+        }
+
+        public void setFileName(String fileName) {
+            this.fileName = fileName;
+        }
+
+        public Material(int materialId) {
+            this.materialId = materialId;
+        }
+
+        public Material(int materialId, String materialName, Module module) {
+            this.materialId = materialId;
+            this.materialName = materialName;
+            this.module = module;
+        }
+
+
+
+
+        public Material(int materialId, String materialName, Module module, String type,
+                Timestamp materialLastUpdate, int materialOrder, String time,
+                String materialDescription, String materialUrl, byte[] materialFile,
+                String fileName) {
+            this.materialId = materialId;
+            this.materialName = materialName;
+            this.module = module;
+            this.type = type;
+            this.materialLastUpdate = materialLastUpdate;
+            this.materialOrder = materialOrder;
+            this.time = time;
+            this.materialDescription = materialDescription;
+            this.materialUrl = materialUrl;
+            this.materialFile = materialFile;
+            this.fileName = fileName;
+        }
+
+
+        public int getMaterialId() {
+            return materialId;
+        }
+
+        public void setMaterialId(int materialId) {
+            this.materialId = materialId;
+        }
+
+        public String getMaterialName() {
+            return materialName;
+        }
+
+        public void setMaterialName(String materialName) {
+            this.materialName = materialName;
+        }
+
+        public Module getModule() {
+            return module;
+        }
+
+        public void setModule(Module module) {
+            this.module = module;
+        }
+
+        public String getType() {
+            return type;
+        }
+
+        public void setType(String type) {
+            this.type = type;
+        }
+
+        public Timestamp getMaterialLastUpdate() {
+            return materialLastUpdate;
+        }
+
+        public void setMaterialLastUpdate(Timestamp materialLastUpdate) {
+            this.materialLastUpdate = materialLastUpdate;
+        }
+
+        public int getMaterialOrder() {
+            return materialOrder;
+        }
+
+        public void setMaterialOrder(int materialOrder) {
+            this.materialOrder = materialOrder;
+        }
+
+        public String getTime() {
+            return time;
+        }
+
+        public void setTime(String time) {
+            this.time = time;
+        }
+
+        public String getMaterialDescription() {
+            return materialDescription;
+        }
+
+        public void setMaterialDescription(String materialDescription) {
+            this.materialDescription = materialDescription;
+        }
+
+        public String getMaterialUrl() {
+            return materialUrl;
+        }
+
+        public void setMaterialUrl(String materialUrl) {
+            this.materialUrl = materialUrl;
+        }
+
+        public byte[] getMaterialFile() {
+            return materialFile;
+        }
+
+        public void setMaterialFile(byte[] materialFile) {
+            this.materialFile = materialFile;
+        }
+
+        @Override
+        public String toString() {
+            return "Material{" + "materialId=" + materialId + ", materialName=" + materialName + ", module=" + module + ", type=" + type + ", materialLastUpdate=" + materialLastUpdate + ", materialOrder=" + materialOrder + ", time=" + time + ", materialDescription=" + materialDescription + ", materialUrl=" + materialUrl + ", materialFile=" + materialFile + '}';
+        }
+
     }
-
-    public void setPdfDataURI(String pdfDataURI) {
-        this.pdfDataURI = pdfDataURI;
-    }
-
-    public Material() {
-    }
-
-    public String getFileName() {
-        return fileName;
-    }
-
-    public void setFileName(String fileName) {
-        this.fileName = fileName;
-    }
-
-    public Material(int materialId) {
-        this.materialId = materialId;
-    }
-
-    public Material(int materialId, String materialName, Module module) {
-        this.materialId = materialId;
-        this.materialName = materialName;
-        this.module = module;
-    }
-
-    
-    
-
-    public Material(int materialId, String materialName, Module module, String type, 
-            Timestamp materialLastUpdate, int materialOrder, String time, 
-            String materialDescription, String materialUrl, byte[] materialFile, 
-            String fileName) {
-        this.materialId = materialId;
-        this.materialName = materialName;
-        this.module = module;
-        this.type = type;
-        this.materialLastUpdate = materialLastUpdate;
-        this.materialOrder = materialOrder;
-        this.time = time;
-        this.materialDescription = materialDescription;
-        this.materialUrl = materialUrl;
-        this.materialFile = materialFile;
-        this.fileName = fileName;
-    }
-
-
-    public int getMaterialId() {
-        return materialId;
-    }
-
-    public void setMaterialId(int materialId) {
-        this.materialId = materialId;
-    }
-
-    public String getMaterialName() {
-        return materialName;
-    }
-
-    public void setMaterialName(String materialName) {
-        this.materialName = materialName;
-    }
-
-    public Module getModule() {
-        return module;
-    }
-
-    public void setModule(Module module) {
-        this.module = module;
-    }
-
-    public String getType() {
-        return type;
-    }
-
-    public void setType(String type) {
-        this.type = type;
-    }
-
-    public Timestamp getMaterialLastUpdate() {
-        return materialLastUpdate;
-    }
-
-    public void setMaterialLastUpdate(Timestamp materialLastUpdate) {
-        this.materialLastUpdate = materialLastUpdate;
-    }
-
-    public int getMaterialOrder() {
-        return materialOrder;
-    }
-
-    public void setMaterialOrder(int materialOrder) {
-        this.materialOrder = materialOrder;
-    }
-
-    public String getTime() {
-        return time;
-    }
-
-    public void setTime(String time) {
-        this.time = time;
-    }
-
-    public String getMaterialDescription() {
-        return materialDescription;
-    }
-
-    public void setMaterialDescription(String materialDescription) {
-        this.materialDescription = materialDescription;
-    }
-
-    public String getMaterialUrl() {
-        return materialUrl;
-    }
-
-    public void setMaterialUrl(String materialUrl) {
-        this.materialUrl = materialUrl;
-    }
-
-    public byte[] getMaterialFile() {
-        return materialFile;
-    }
-
-    public void setMaterialFile(byte[] materialFile) {
-        this.materialFile = materialFile;
-    }
-
-    @Override
-    public String toString() {
-        return "Material{" + "materialId=" + materialId + ", materialName=" + materialName + ", module=" + module + ", type=" + type + ", materialLastUpdate=" + materialLastUpdate + ", materialOrder=" + materialOrder + ", time=" + time + ", materialDescription=" + materialDescription + ", materialUrl=" + materialUrl + ", materialFile=" + materialFile + '}';
-    }
-
-}
