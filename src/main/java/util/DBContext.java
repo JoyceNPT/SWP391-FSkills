cd5eeb62679ae7d5c5d66a07a1481036a7c7444f
ThinhNPCE170008 <ThinhNPCE170008@fpt.edu.vn>
2025-07-25 15:30:21 +0700

    Update package DBContext

--- src/main/java/util/DBContext.java
+++ src/main/java/util/DBContext.java
@@ -1,5 +1,7 @@
 package util;
 
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
 import java.sql.Connection;
 import java.sql.DatabaseMetaData;
 import java.sql.DriverManager;
@@ -15,37 +17,33 @@ public class DBContext {
     public Connection conn = null;
 
     // Connect Local
-     public DBContext() {
-         try {
-             Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
- //            String dbURL = "jdbc:sqlserver://javademo-server.database.windows.net:1433;"
- //                    + "databaseName=FLearn;"
- //                    + "user=adminuser@javademo-server;"
- //                    + "password=Group3SWP391!;"
- //                    + "encrypt=true;trustServerCertificate=false;loginTimeout=30;";
-             String dbURL = "jdbc:sqlserver://localhost:1433;"
-                     + "databaseName=FLearn;"
-                     + "user=sa;"
-                     + "password=123456;"
-                     + "encrypt=false;trustServerCertificate=true;loginTimeout=30;";
-             conn = DriverManager.getConnection(dbURL);
+    public DBContext() {
+        try {
+            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+            String dbURL = "jdbc:sqlserver://localhost:1433;"
+                    + "databaseName=FLearn;"
+                    + "user=sa;"
+                    + "password=123456;"
+                    + "encrypt=false;trustServerCertificate=true;loginTimeout=30;";
+            conn = DriverManager.getConnection(dbURL);
 
-             if (conn != null) {
-                 DatabaseMetaData dm = (DatabaseMetaData) conn.getMetaData();
-                 System.out.println("Driver name: " + dm.getDriverName());
-                 System.out.println("Driver version: " + dm.getDriverVersion());
-                 System.out.println("Product name: "
-                         + dm.getDatabaseProductName());
-                 System.out.println("Product version: "
-                         + dm.getDatabaseProductVersion());
-             }
-         } catch (SQLException ex) {
-         } catch (ClassNotFoundException ex) {
-             Logger.getLogger(DBContext.class.getName()).log(Level.SEVERE, null, ex);
-         }
-     }
+            if (conn != null) {
+                DatabaseMetaData dm = (DatabaseMetaData) conn.getMetaData();
+                System.out.println("Driver name: " + dm.getDriverName());
+                System.out.println("Driver version: " + dm.getDriverVersion());
+                System.out.println("Product name: "
+                        + dm.getDatabaseProductName());
+                System.out.println("Product version: "
+                        + dm.getDatabaseProductVersion());
+            }
+        } catch (SQLException ex) {
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(DBContext.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+}
 
-    // Conect JDBC
+    // Conect JDBC Connect Render
 //   public DBContext() {
 //       try {
 //           Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
@@ -85,4 +83,63 @@ public class DBContext {
 //           Logger.getLogger(DBContext.class.getName()).log(Level.SEVERE, null, ex);
 //       }
 //   }
-}
+//}
+
+//    private static HikariDataSource ds;
+//
+//    static {
+//        try {
+//            // Lấy thông tin từ biến môi trường
+//            String host = System.getenv("DB_HOST");
+//            String port = System.getenv("DB_PORT");
+//            String dbName = System.getenv("DB_NAME");
+//            String user = System.getenv("DB_USER");
+//            String pass = System.getenv("DB_PASS");
+//
+//            String jdbcUrl = "jdbc:sqlserver://" + host + ":" + port + ";"
+//                    + "databaseName=" + dbName + ";"
+//                    + "encrypt=true;"
+//                    + "trustServerCertificate=false;"
+//                    + "loginTimeout=30;";
+//
+//            System.out.println("DEBUG HIKARICP START");
+//            System.out.println("DB_HOST = " + host);
+//            System.out.println("DB_USER = " + user);
+//            System.out.println("DB_NAME = " + dbName);
+//            System.out.println("JDBC URL = " + jdbcUrl);
+//            System.out.println("DEBUG HIKARICP END");
+//
+//            HikariConfig config = new HikariConfig();
+//            config.setJdbcUrl(jdbcUrl);
+//            config.setUsername(user);
+//            config.setPassword(pass);
+//            config.setDriverClassName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+//
+//            config.setMaximumPoolSize(10);
+//            config.setMinimumIdle(2);
+//            config.setIdleTimeout(30000);
+//            config.setConnectionTimeout(10000);
+//            config.setValidationTimeout(5000);
+//            config.setLeakDetectionThreshold(15000); // phát hiện connection bị quên đóng
+//
+//            ds = new HikariDataSource(config);
+//
+//        } catch (Exception ex) {
+//            ex.printStackTrace();
+//        }
+//    }
+//
+//    public DBContext() {
+//        try {
+//            conn = ds.getConnection();
+//        } catch (SQLException e) {
+//            e.printStackTrace();
+//        }
+//    }
+//
+//    public static void closePool() {
+//        if (ds != null && !ds.isClosed()) {
+//            ds.close();
+//        }
+//    }
+//}
