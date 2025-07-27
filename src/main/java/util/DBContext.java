f145838c93f857a226a80e0787f5b2ba4fdfa5b9
ThinhNPCE170008 <ThinhNPCE170008@fpt.edu.vn>
2025-07-27 19:07:42 +0700

    setup deploy

--- src/main/java/util/DBContext.java
+++ src/main/java/util/DBContext.java
@@ -1,7 +1,5 @@
 package util;
 
-import com.zaxxer.hikari.HikariConfig;
-import com.zaxxer.hikari.HikariDataSource;
 import java.sql.Connection;
 import java.sql.DatabaseMetaData;
 import java.sql.DriverManager;
@@ -41,29 +39,45 @@ public class DBContext {
 //            Logger.getLogger(DBContext.class.getName()).log(Level.SEVERE, null, ex);
 //        }
 //    }
-    
     public DBContext() {
         try {
             Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
-            String dbURL = "jdbc:sqlserver://localhost:1433;"
+
+            String dbURL = "jdbc:sqlserver://148.230.96.68:1433;"
                     + "databaseName=FLearn;"
                     + "user=sa;"
-                    + "password=Group3-FPTU-NPT;"
-                    + "encrypt=false;trustServerCertificate=true;loginTimeout=30;";
+                    + "password=FPT_Group3_SE1815;"
+                    + "encrypt=false;"
+                    + "trustServerCertificate=true;"
+                    + "loginTimeout=30;";
+
             conn = DriverManager.getConnection(dbURL);
 
             if (conn != null) {
-                DatabaseMetaData dm = (DatabaseMetaData) conn.getMetaData();
+                DatabaseMetaData dm = conn.getMetaData();
+                System.out.println("Connected to DB");
                 System.out.println("Driver name: " + dm.getDriverName());
                 System.out.println("Driver version: " + dm.getDriverVersion());
-                System.out.println("Product name: "
-                        + dm.getDatabaseProductName());
-                System.out.println("Product version: "
-                        + dm.getDatabaseProductVersion());
+                System.out.println("Product name: " + dm.getDatabaseProductName());
+                System.out.println("Product version: " + dm.getDatabaseProductVersion());
+            } else {
+                System.err.println("conn == null");
+            }
+
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(DBContext.class.getName()).log(Level.SEVERE, "Not Found JDBC Driver", ex);
+        } catch (SQLException ex) {
+            Logger.getLogger(DBContext.class.getName()).log(Level.SEVERE, "Error Connect DB", ex);
+        }
+    }
+
+    public void close() {
+        try {
+            if (conn != null && !conn.isClosed()) {
+                conn.close();
             }
         } catch (SQLException ex) {
-        } catch (ClassNotFoundException ex) {
-            Logger.getLogger(DBContext.class.getName()).log(Level.SEVERE, null, ex);
+            Logger.getLogger(DBContext.class.getName()).log(Level.SEVERE, "Error: Close DB", ex);
         }
     }
 }
